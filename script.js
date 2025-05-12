0082d7e26c5431929a6dfa9008a67748e37c9770
ajay123679 <65329197+ajay123679@users.noreply.github.com>
2025-05-12 10:01:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ function Car(make, model) {
 	this.model=model;
 }
 Car.prototype.getMakeModel=function(){
-	return this make+" "+this.model;
+	return this.make+" "+this.model;
 }
 function SportsCar(make, model, topSpeed) {
 	Car.call(this,make,model);
 	this.topSpeed=topSpeed;
 }
-SportsCar.prototype=object.create(Car.prototype);
+SportsCar.prototype=Object.create(Car.prototype);
 SportsCar.prototype.constructor=SportsCar;
 SportsCar.prototype.getTopSpeed=function(){
 	return this.topSpeed;
